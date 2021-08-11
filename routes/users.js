e0f40b8b1e95b4a98e73430939fcbe023d55ff77
NicatMirzaev <nicatmirzoev111@gmail.com>
2021-08-12 00:05:56 +0400

    added user profile update route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const {
+  INVALID_TOKEN,
+  INVALID_SYNTAX,
+  BIO_LENGTH,
+  NAME_LENGTH,
+} = require("../lib/error-codes");
 
 module.exports = (User) => {
-  router.get("/me", async (req, res, next) => {
-    if (!req.user) return res.status(400).send({ error: "Invalid token." });
+  router.post("/update", async (req, res) => {
+    const { bio, displayName, profileImg } = req.body;
+    if (!req.user) return res.status(400).send(INVALID_TOKEN);
     const user = await User.findById(req.user.id);
-    if (!user) return res.status(401).send({ error: "Invalid credentials." });
+    if (!user) return res.status(401).send(INVALID_TOKEN);
+    if (
+      bio === undefined ||
+      displayName === undefined ||
+      profileImg === undefined
+    )
+      return res.status(400).send(INVALID_SYNTAX);
+    if (bio.length > 300) return res.status(400).send(BIO_LENGTH);
+    if (displayName.length < 3 || displayName.length > 50)
+      return res.status(400).send(NAME_LENGTH);
+
+    user.bio = bio;
+    user.displayName = displayName;
+    user.profileImg = profileImg;
+    await user.save();
+    return res.status(200).send(user);
+  });
+  router.get("/me", async (req, res) => {
+    if (!req.user) return res.status(400).send(INVALID_TOKEN);
+    const user = await User.findById(req.user.id);
+    if (!user) return res.status(401).send(INVALID_TOKEN);
     return res.status(200).send(user);
   });
   return router;
